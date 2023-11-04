40328453b6e1c63202b8508b86ec714cf6adc46b
sametaydinq <sametaydinq@gmail.com>
2023-11-04 03:06:51 +0300

    styling the skeleton

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import "./App.css"
+import {AiOutlineDelete} from "react-icons/ai"
+import {BsCheckLg} from "react-icons/bs"
+
 
 function App() {
+  const [isCompleteScreen,setIsCompleteScreen] = useState(false);
+
   return (
    <div className="App">
     <h1>My Todos</h1>
@@ -8,10 +14,10 @@ function App() {
       <div className="todo-wrapper">
         <div className="todo-input">
           <div className="todo-input-item">
-            <div>
+            
               <label>Title</label>
               <input type= "text" placeholder="What is the task title?"b/>
-            </div>
+          </div>
 
             <div className="todo-input-item">
               <label>Dexcription</label>
@@ -24,23 +30,28 @@ function App() {
           </div>
 
           <div className="btn-area">
-            <button>Todo</button>
-            <button>Completed</button>
+            <button className={`secondaryBtn ${isCompleteScreen === false && "active"}`} onClick={()=>setIsCompleteScreen(false)}> Todo</button>
+            <button className={`secondaryBtn ${isCompleteScreen === true && "active"}`} onClick={()=>setIsCompleteScreen(true)}>Completed</button>
           </div>
 
           <div className="todo-list">
 
-          <div className="todo-list-item">
-            <div>
-              <h1>Task 1</h1>
+            <div className="todo-list-item">
+          <div>
+              <h3>Task 1</h3>
               <p>Description</p>
+
           </div>
+            <div>
+              <AiOutlineDelete className="icon"/>
+              <BsCheckLg className="check-icon"/>
+              
+            </div>
+            </div>
         </div>
       </div>
-
-      </div>
     </div>
-   </div>
+   
   );
 }
 
